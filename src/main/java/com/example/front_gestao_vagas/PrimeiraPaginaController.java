a4df702ce5c4456c27678e52fcfc88bd68d84437
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-03-15 14:19:34 -0300

    Recebendo parâmetros da controller no HTML

--- src/main/java/com/example/front_gestao_vagas/PrimeiraPaginaController.java
+++ src/main/java/com/example/front_gestao_vagas/PrimeiraPaginaController.java
@@ -1,6 +1,7 @@
 package com.example.front_gestao_vagas;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class PrimeiraPaginaController {
     @GetMapping("/home")
-    public  String primeiraPagina() {
+    public  String primeiraPagina(Model model){
+        model.addAttribute("mensagemDaController", "Primeira mensagem da controller!");
         return "primeiraPagina";
+    }
+
+    @GetMapping("/login")
+    public  String login(Model model){
+        return "login";
     }
 }
