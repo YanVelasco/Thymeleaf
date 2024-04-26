fadc45fb5540d6f54a90b6fb92450c6f51f3a9f4
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-04-26 10:37:09 -0300

    Inserindo o Spring Security

---
+++ src/main/java/com/example/front_gestao_vagas/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.front_gestao_vagas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests (auth -> {
+            auth.requestMatchers ("/candidate/login").permitAll ();
+            auth.anyRequest ().authenticated ();
+        }).formLogin (formLogin -> formLogin.loginPage ("/candidate/login").permitAll ());
+        return http.build ();
+    }
+}
